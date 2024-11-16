5e5c026e325eeec9af124b4fc9f51bf4ec655a08
Gabriel345 <gabrielmzenerato242@gmail.com>
2024-11-16 16:46:43 -0300

    subindom logica

--- controller/eventController.js
+++ controller/eventController.js
@@ -1,42 +1,39 @@
 const Event = require('../models/event');
 const path = require('path');
 
-
-
 exports.createEvent = async (req, res) => {
   try {
-      const { title, type, description, date } = req.body;
+    const { title, type, description, date } = req.body;
 
-      // Verificar se o usuário está autenticado
-      if (!req.session.userId) {
-          return res.status(401).json({ message: 'Usuário não autenticado' });
-      }
+    // Verificar se o usuário está autenticado
+    if (!req.session.userId) {
+      return res.status(401).json({ message: 'Usuário não autenticado' });
+    }
 
-      // Verificar se a imagem de capa foi enviada
-      if (!req.file) {
-          return res.status(400).json({ message: 'Imagem de capa não enviada' });
-      }
+    // Verificar se a imagem de capa foi enviada
+    if (!req.file) {
+      return res.status(400).json({ message: 'Imagem de capa não enviada' });
+    }
 
-      const coverImagePath = path.relative(path.join(__dirname, '..', 'public', 'uploads'), req.file.path);
-    
-      const organizer = req.session.userId;
+    const coverImagePath = path.relative(path.join(__dirname, '..', 'public', 'uploads'), req.file.path);
+    const organizer = req.session.userId;
 
-      const newEvent = new Event({
-          title,
-          type,
-          description,
-          date,
-          organizer,
-          coverImage: coverImagePath
-      });
-      await newEvent.save();
+    const newEvent = new Event({
+      title,
+      type,
+      description,
+      date,
+      organizer,
+      coverImage: coverImagePath
+    });
 
-      
-      res.redirect('/');
+    await newEvent.save();
+    res.status(201).json({ message: 'Evento criado com sucesso', event: newEvent });
   } catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
+
 exports.getAllEvents = async (req, res) => {
   try {
     const events = await Event.find();
@@ -45,6 +42,7 @@ exports.getAllEvents = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 exports.getEventById = async (req, res) => {
   try {
     const event = await Event.findById(req.params.id);
@@ -68,87 +66,35 @@ exports.updateEvent = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-exports.renderEditEventPage = async (req, res) => {
-  try {
-    // Encontre o evento pelo ID fornecido na rota
-    const event = await Event.findById(req.params.id);
-    
-    // Verifique se o usuário está autenticado
-    const userId = req.session.userId;
-    if (!userId) {
-      return res.status(401).json({ message: 'Usuário não autenticado' });
-    }
 
-    // Renderize a página de edição de evento, passando o evento encontrado e o userId
-    res.render('editEvent', { event, userId });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.editEvent = async (req, res) => {
-  try {
-    const eventId = req.params.id;
-    const { title, type, description, date } = req.body;
-    let updatedData = {
-      title,
-      type,
-      description,
-      date
-    };
-
-    // Verificar se há um novo arquivo de imagem enviado
-    if (req.file) {
-      const coverImagePath = path.relative(path.join(__dirname, '..', 'public', 'uploads'), req.file.path);
-      updatedData.coverImage = coverImagePath;
-    }
-
-    const updatedEvent = await Event.findByIdAndUpdate(eventId, updatedData, { new: true });
-
-    if (!updatedEvent) {
-      return res.status(404).json({ message: 'Evento não encontrado' });
-    }
-
-    res.redirect('/profile'); // Redireciona para a página de perfil ou outra página após a edição
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
 exports.deleteEvent = async (req, res) => {
   try {
     const deletedEvent = await Event.findByIdAndDelete(req.params.id);
     if (!deletedEvent) {
-      // Se o evento não for encontrado, redirecione para a página de perfil
-      return res.redirect('/profile');
+      return res.status(404).json({ message: 'Evento não encontrado' });
     }
-    // Se a exclusão for bem-sucedida, redirecione para a página de perfil
-    return res.redirect('/profile');
-  } catch (error) {
-    // Em caso de erro, redirecione para a página de perfil
-    return res.redirect('/profile');
-  }
-};
-exports.getRecentEvents = async (req, res) => {
-  try {
-    // Recupere os eventos do banco de dados e ordene por data em ordem decrescente
-    const events = await Event.find().sort({ date: -1 }).limit(3);
-
-    // Renderize a visualização do carrossel passando os eventos para exibir
-    res.render('carousel', { events });
+    res.status(200).json({ message: 'Evento deletado com sucesso' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getRecentEvents = async (req, res) => {
+  try {
+    const events = await Event.find().sort({ date: -1 }).limit(3);
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 exports.getEventDetails = async (req, res) => {
   try {
     const event = await Event.findById(req.params.id).populate('organizer');
-    
     if (!event) {
       return res.status(404).json({ message: 'Evento não encontrado' });
     }
-    
-    const userId = req.session.userId;
-    res.render('event-details', { event, userId }); // Renderiza uma view com os detalhes do evento
+    res.status(200).json(event);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -166,8 +112,6 @@ exports.registerForEvent = async (req, res) => {
 
     // Encontrar o evento pelo ID
     const event = await Event.findById(eventId);
-
-    // Verificar se o evento existe
     if (!event) {
       return res.status(404).json({ message: 'Evento não encontrado' });
     }
@@ -182,13 +126,12 @@ exports.registerForEvent = async (req, res) => {
     event.participants.push({ user: userId });
     await event.save();
 
-    // Redirecionar o usuário de volta para a página do evento
-    res.redirect(`/events/${eventId}`);  // Substitua com a rota correta para a página de detalhes do evento
-
+    res.status(200).json({ message: 'Usuário registrado no evento', event });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 exports.unregisterFromEvent = async (req, res) => {
   try {
     const eventId = req.params.eventId;
@@ -201,8 +144,6 @@ exports.unregisterFromEvent = async (req, res) => {
 
     // Encontrar o evento pelo ID
     const event = await Event.findById(eventId);
-
-    // Verificar se o evento existe
     if (!event) {
       return res.status(404).json({ message: 'Evento não encontrado' });
     }
@@ -211,15 +152,8 @@ exports.unregisterFromEvent = async (req, res) => {
     event.participants = event.participants.filter(participant => !participant.user.equals(userId));
     await event.save();
 
-    // Redirecionar o usuário de volta para a página do evento ou para o perfil
-    res.redirect(`/events/${eventId}`);  // Substitua com a rota correta para a página de detalhes do evento
-
+    res.status(200).json({ message: 'Usuário desregistrado do evento', event });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-};
-exports.renderCreateEventPage = (req, res) => {
-  const userId = req.session.userId; // Supondo que userId está disponível na sessão
-
-  res.render('create-event', { userId: userId });
 };
